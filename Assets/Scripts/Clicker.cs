6ec9b7a5c8abf70ce0e93eb4ec3057d39badbea8
Divya Thakur <rewire@gmail.com>
2016-04-09 21:24:50 -0400

    coin sounds and code to play it

---
+++ Assets/Scripts/Clicker.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Clicker : MonoBehaviour {
+
+	GameObject camera;
+
+	// Use this for initialization
+	void Start () {
+		camera = GameObject.FindGameObjectsWithTag("MainCamera")[0];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var dist = Mathf.Abs(Vector3.Distance(
+			this.transform.position, 
+			camera.transform.position));
+
+		Debug.Log(dist);
+		if (dist < 1) {
+			var audio = GetComponent<AudioSource>();
+			audio.Play();
+
+			GetComponent<Renderer>().enabled = false;
+			Destroy(this);
+//			Application.Quit();
+		}
+	}
+}
